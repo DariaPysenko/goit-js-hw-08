7eefc1ce0fc034b1fa26179be42e676aec7efc5a
DariaPysenko <83023041+DariaPysenko@users.noreply.github.com>
2022-01-10 23:55:06 +0200

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
+const createMarkup = ({ original, preview, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+        ;
+};
+
+
+const renderGallery = () => {
+    const markup = galleryItems.map((item) => 
+        createMarkup(item)).join('');
+    return markup;
+
+    
+};
+
+const onModalClick = (event) => {
+    event.preventDefault();
+    if (event.target === event.currentTarget) return;
+    const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} alt=${event.target.alt}>
+`,
+        {
+            onShow: (instance) => {
+                window.addEventListener('keydown', (event) => {
+                    if (event.code === 'Escape') instance.close();
+                });
+            },
+                
+            onClose: (instance) => {
+                window.removeEventListener('keydown', (event) => {
+                    if (event.code === 'Escape') instance.close();
+                });
+            },
+        }
+   
+    );
+    instance.show();
+};
+galleryRef.insertAdjacentHTML('beforeend', renderGallery());
+galleryRef.addEventListener('click', onModalClick);
 console.log(galleryItems);
+new SimpleLightbox('.gallery a', { captionType: 'attr', captionDelay: 250, captionsData:    'alt'});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
 
+
+import throttle from 'lodash.throttle'; // библиотека
+const STORAGE_KEY = 'feedback-form-state'; //объект данных для полей ввода
+const feedbackFormData = {};
+// элементы формы
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+//вешаем слушателя
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+populateOnFormInput();
+//ф-ция ввода
+function onFormInput(e) {
+  feedbackFormData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+}
+//ф-ция формы
+function onFormSubmit(e) {
+    // без дефолтной перезагрузки
+    e.preventDefault();
+    // очистить перезагрузить форму
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(feedbackFormData);
+}
+//ф-ция при релоуде и вернуть сохраненные данные
+function populateOnFormInput() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedMessage) {
+    refs.input.value = savedMessage.email || '';
+    refs.textarea.value = savedMessage.message || '';
+    feedbackFormData.email = refs.input.value;
+    feedbackFormData.message = refs.textarea.value;
+  }
+}
+
+//все
+ 
