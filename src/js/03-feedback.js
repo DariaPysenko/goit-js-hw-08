18a91380a3905e918985ee2825b6951522d118ad
DariaPysenko <83023041+DariaPysenko@users.noreply.github.com>
2022-01-21 11:31:46 +0200

    upd feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,43 @@
 
 
-import throttle from 'lodash.throttle'; // библиотека
-const STORAGE_KEY = 'feedback-form-state'; //объект данных для полей ввода
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state'; 
 const feedbackFormData = {};
-// элементы формы
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
-//вешаем слушателя
+
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 populateOnFormInput();
-//ф-ция ввода
+
 function onFormInput(e) {
   feedbackFormData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
+  
 }
-//ф-ция формы
-function onFormSubmit(e) {
-    // без дефолтной перезагрузки
-    e.preventDefault();
-    // очистить перезагрузить форму
 
-  localStorage.removeItem(STORAGE_KEY);
+function onFormSubmit(e) {
+
+  e.preventDefault();
   const emailValue = refs.input.value;
   const messageValue = refs.textarea.value;
-
   if (emailValue === '' || messageValue === '') {
     return alert('Заполните обязательные поля Email и Message:');
   }
   console.log(feedbackFormData);
   e.currentTarget.reset();
-
+  localStorage.clear();
+  
   
 }
-//ф-ция при релоуде и вернуть сохраненные данные
+
 function populateOnFormInput() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
   if (savedMessage) {
     refs.input.value = savedMessage.email || '';
     refs.textarea.value = savedMessage.message || '';
@@ -49,5 +46,5 @@ function populateOnFormInput() {
   }
 }
 
-//все
+
  
